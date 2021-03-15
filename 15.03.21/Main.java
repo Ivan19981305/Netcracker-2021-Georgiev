4230dc75b0540e9307c428f78d573e900d4adf33
Ivan19981305 <Ivan19981305@yandex.ru>
2021-03-15 18:15:47 +0300

    second commit 15.03

--- 15.03.21/Main.java
+++ 15.03.21/Main.java
@@ -1,9 +1,26 @@
 public class Main{
     public static void main(String arg[]){
+        
+        Something object = new Something();
+        
+               
+        /*
         Counter.countNow();
         Counter.countNow();
+        */
     }
 }
+
+
+/*консоль:
+Ivan@Ivan19981305 MINGW64 /d/repos/Netcracker-2021-Georgiev/15.03.21 (main)
+$ javac Main.java
+
+Ivan@Ivan19981305 MINGW64 /d/repos/Netcracker-2021-Georgiev/15.03.21 (main)
+$ java Main
+static block
+Constructor
+*/
 
 class Counter{
     static int count = 0;
@@ -13,4 +30,14 @@ class Counter{
     }
 }
 
-
+class Something{
+    static int count = 0;
+    
+    public Something(){
+        System.out.println("Constructor");
+    }
+    
+    static{
+        System.out.println("static block");
+    }
+}
