64858fae856cda31b29b0976629711b4758886ee
Ivan19981305 <Ivan19981305@yandex.ru>
2021-03-19 17:38:07 +0300

    first

---
+++ src/ivge/Main.java
@@ -0,0 +1,12 @@
+package ivge;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Student first = new Student("name", 1, 'a');
+        Student second = new Student("name", 1);
+        Student third = new Student("name", 1);
+    }
+}
