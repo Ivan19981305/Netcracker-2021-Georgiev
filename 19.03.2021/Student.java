d93e0932eb4db22e91b8d4fa68b29fbb4201751d
Ivan19981305 <Ivan19981305@yandex.ru>
2021-03-19 19:06:06 +0300

    throw

--- 19.03.2021/Student.java
+++ 19.03.2021/Student.java
@@ -1,25 +1,34 @@
 package ivge;
 
-public class Student {
-    private String name = "";
-    private int number = 1;
-    private char letter = 'a';
+import java.util.Arrays;
 
-    Student(String name)
-    {
+public class Student implements Cloneable {
+    private String name = "";
+    private Integer number = 1;
+    private char letter = 'a';
+    private int[] marks = {};
+
+    Student(String name) {
         this(name, 1, 'a');
     }
 
-    Student(String name, int number)
-    {
+    Student(String name, Integer number) {
         this(name, number, 'a');
     }
 
-    Student(String name, int number, char letter)
-    {
+    Student(String name, Integer number, char letter) {
+
+        this(name, number, 'a', new int[]{});
+    }
+
+    Student(String name, Integer number, char letter, int[] marks) {
         this.name = name;
         this.number = number;
         this.letter = letter;
+
+        checkMarks(marks);
+
+        this.marks = Arrays.copyOf(marks, marks.length);
     }
 
     public char getLetter() {
@@ -30,11 +39,11 @@ public class Student {
         this.letter = letter;
     }
 
-    public int getNumber() {
+    public Integer getNumber() {
         return number;
     }
 
-    public void setNumber(int number) {
+    public void setNumber(Integer number) {
         this.number = number;
     }
 
@@ -45,4 +54,24 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int[] getMarks() {
+        return Arrays.copyOf(marks, marks.length);
+    }
+
+    public void setMarks(int[] marks) {
+        checkMarks(marks);
+        this.marks = Arrays.copyOf(marks, marks.length);
+    }
+
+
+    private void checkMarks(int[] marks){
+        for(int mark : marks)
+            if (!(2 <= mark && mark <= 5))
+                throw new IllegalArgumentException();
+    }
+
+
+
+
 }
--- src/ivge/Main.java
+++ src/ivge/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        int[] marks = {2, 3, 2, 1};
+        Student buggyBoy = new Student("name", 1, 'a', marks);
+
         Student first = new Student("name", 1, 'a');
         Student second = new Student("name", 1);
-        Student third = new Student("name", 1);
+        Student third = new Student("name");
     }
 }
