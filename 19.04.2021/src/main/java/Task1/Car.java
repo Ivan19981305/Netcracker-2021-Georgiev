c9e39618f5529d4938f47dc3874651bd9105fbe2
Ivan19981305 <Ivan19981305@yandex.ru>
2021-04-19 18:59:31 +0300

    19.04.2021 task 1

---
+++ 19.04.2021/src/main/java/Task1/Car.java
@@ -0,0 +1,78 @@
+package Task1;
+
+public class Car {
+    private String wheels;
+    private String engine;
+    private String carBody;
+
+    private Boolean trunk;
+
+
+    public Car(String wheels, String engine, String carBody) {
+        this.wheels = wheels;
+        this.engine = engine;
+        this.carBody = carBody;
+        if (carBody != null && carBody.charAt(0) == '1')
+        {
+            trunk = true;
+        }
+        if (carBody != null && carBody.charAt(0) == '2')
+        {
+            trunk = false;
+        }
+    }
+
+    public Car() {
+
+    }
+
+    public String getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(String wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getCarBody() {
+        return carBody;
+    }
+
+    public void setCarBody(String carBody) {
+        if (carBody != null && carBody.charAt(0) == '1')
+        {
+            trunk = true;
+        }
+        if (carBody != null && carBody.charAt(0) == '2')
+        {
+            trunk = false;
+        }
+        this.carBody = carBody;
+    }
+
+    public Boolean getTrunk() {
+        return trunk;
+    }
+
+    public void setTrunk(Boolean trunk) {
+        this.trunk = trunk;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "wheels='" + wheels + '\'' +
+                ", engine='" + engine + '\'' +
+                ", carBody='" + carBody + '\'' +
+                ", trunk=" + trunk +
+                '}';
+    }
+}
--- 19.04.2021/src/main/java/Task1/Main.java
+++ 19.04.2021/src/main/java/Task1/Main.java
@@ -1,22 +1,26 @@
 package Task1;
 
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.*;
-import java.net.URL;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //"D:\\repos\\Netcracker-2021-Georgiev\\16.04.2021\\src\\main\\resources\\test.txt"
-        try(InputStream fileInputStream = new URL("https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F:%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0").openStream();
-            FileOutputStream fileOutputStream = new FileOutputStream("D:\\repos\\Netcracker-2021-Georgiev\\16.04.2021\\src\\main\\resources\\test2.txt")) {
-            byte[] someByte = new byte[1024];
-            int length = 0;
-            while ((length = fileInputStream.read(someByte)) != -1)
-                fileOutputStream.write(someByte);
-            fileOutputStream.flush();
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        } catch (IOException e) {
-            e.printStackTrace();
+        Car car = new Car("4 wheels", "Big", "1st type");
+
+        try (OutputStream outputStream = new FileOutputStream("text.json");
+             InputStream inputStream = new FileInputStream("text.json")){
+            objectMapper.writeValue(outputStream, car);
+
+            Car car1;
+
+            car1 = objectMapper.readValue(inputStream, Car.class);
+
+            System.out.println(car1);
         }
     }
 }
