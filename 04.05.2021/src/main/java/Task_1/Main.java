4708a72617f7007801f2721f9bc5d5bb568632e2
Ivan19981305 <Ivan19981305@yandex.ru>
2021-05-04 17:45:27 +0300

    Task 1

--- 04.05.2021/src/main/java/Task_1/Main.java
+++ 04.05.2021/src/main/java/Task_1/Main.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -21,7 +21,6 @@ public class Main {
 class JavaConfig{
 
     @Bean
-    @Lazy
     public Test mainCharacter(){
         Test test = new Test();
         test.setName("Test 1");
